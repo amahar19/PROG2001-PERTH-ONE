fc00d0b53b875c7b85f9b4b064f8c0ee093444d1
Aasish Maharjan <maharjanaasish@gmail.com>
2024-04-05 11:02:39 +0800

    Implemented SceneLoader script in my scenes
    ensured that the SceneLoader script is added to all interactive elements, including buttons and any other elements that trigger scene transitions. This script facilitates seamless loading between scenes upon interaction. Also Verifed that every interactive element is properly configured to utilize the SceneLoader functionality when clicked.

---
+++ Assets/AasishMaharjan/Scripts/SceneLoader.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void ClickToPlay()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+
+    public void ClickToHome()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+    public void ClickToOptions()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+
+    public void ClickToExit()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+    public void ClickToTryAgain()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+
+    public void ClickToPlayAgain()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+
+    public void ClickToHelp()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+
+    public void ClickToCredits()
+    {
+        SceneManager.LoadScene(7);
+    }
+
+    public void ClickToSound()
+    {
+        SceneManager.LoadScene(8);
+    }
+
+    public void ClickToMySceneHome()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+}
