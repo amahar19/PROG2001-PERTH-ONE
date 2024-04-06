292895f0cbdd97b7caa06f2907f6f8930c3af150
Sharanbabu1 <162317092+Sharanbabu1@users.noreply.github.com>
2024-04-06 12:44:11 +0800

    added player controller and camera controller script
    player controller and camera controller

--- Assets/Sharan Adhikari/Scripts/PlayerController.cs
+++ Assets/Sharan Adhikari/Scripts/PlayerController.cs
@@ -8,6 +8,12 @@ public class PlayerController : MonoBehaviour
    private CharacterController controller;
    private Vector3 direction;
    public float forwardSpeed;
+   private int desiredLane = 1; // 0 left, 1 middle, 2 right
+   public float laneDistance = 4; //distance between 2 lanes
+   public float jumpForce; 
+   public float Gravity = -20;
+
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -17,10 +23,63 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         direction.z =forwardSpeed;
+
         
+        if(controller.isGrounded)
+        {
+          direction.y = -1;
+          if(Input.GetKeyDown(KeyCode.UpArrow))
+          {
+            Jump();
+          }
+        }
+        else
+        {
+          direction.y += Gravity * Time.deltaTime;
+        }
+
+
+
+    //the default lane which player will be on
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            desiredLane++;
+            if(desiredLane ==3)
+               desiredLane = 2;
+        }
+
+         if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            desiredLane--;
+            if(desiredLane ==-1)
+               desiredLane = 0;
+        }
+
+        //where player will be on future
+
+      Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+
+      if(desiredLane == 0)
+      {
+        targetPosition += Vector3.left * laneDistance;
+      }
+       else if (desiredLane == 2)
+      {
+        targetPosition += Vector3.right * laneDistance;
+      }
+
+      transform.position = targetPosition;
     }
+
     private void FixedUpdate()
     {
         controller.Move(direction*Time.fixedDeltaTime);
+    }
+
+    private void Jump()
+    {
+      direction.y = jumpForce;
+
     }
 }
