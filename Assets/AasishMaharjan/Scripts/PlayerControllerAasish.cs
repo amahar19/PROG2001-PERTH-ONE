c170f5f371d57a8c2c4e9dd19cfa7ac24a151d49
Aasish Maharjan <maharjanaasish@gmail.com>
2024-04-12 01:59:35 +0800

    Added, updated, fixed Character controller and camera controller on game object
    Added, updated, fixed Character controller and camera controller on game object.
    
    Also changed worldspace to screen overlay mode

--- Assets/AasishMaharjan/Scripts/PlayerControllerAasish.cs
+++ Assets/AasishMaharjan/Scripts/PlayerControllerAasish.cs
@@ -7,7 +7,7 @@ public class PlayerControllerAasish : MonoBehaviour
     public CharacterController controller;
     public Animator animator;
 
-
+    private float moveSpeed = 4f;
 
 
     // Start is called before the first frame update
@@ -29,10 +29,15 @@ public class PlayerControllerAasish : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
 
         Vector3 dir = new Vector3(horizontal, 0f, vertical).normalized;
+        Vector3 velocity = moveSpeed * Time.deltaTime * dir;
 
         if(dir.magnitude >= 0.1f)
         {
             transform.rotation = Quaternion.LookRotation(dir);
+
+            controller.Move(velocity);
         }
+
+        animator.SetFloat("Speed", velocity.magnitude);
     }
 }
