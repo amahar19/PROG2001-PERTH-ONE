2bbbf9874ce7b49ee2375bf6752674fb79329e08
Devbhuje <99317603+Devbhuje@users.noreply.github.com>
2024-04-12 13:52:42 +0800

    a

--- Assets/Dev Bhujel Game/Scripts/Angle/DevCameraController.cs
+++ Assets/Dev Bhujel Game/Scripts/Angle/DevCameraController.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class DevCameraController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform target;
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        transform.LookAt(target);
     }
 }
--- Assets/Sharan Adhikari/Scripts/SharanCameraController.cs
+++ Assets/Sharan Adhikari/Scripts/SharanCameraController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class SharanCameraController : MonoBehaviour
 {
     public Transform target;
     private Vector3 offset;
@@ -10,15 +10,19 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position - target.position;
+       
         
+    }
+    void StartController()
+    {
+        offset = transform.position - target.position;
     }
 
     
 
     // Update is called once per frame
 
-    void LateUpdate()
+    void lateUpdate()
     {
         Vector3 newPosition = new Vector3(transform.position.x,transform.position.y,offset.z+target.position.z);
         transform.position = Vector3.Lerp(transform.position, newPosition, 10 * Time.deltaTime);
