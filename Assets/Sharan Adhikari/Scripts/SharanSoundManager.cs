432a11512f2fb9bcabb8b049694c80bfe70bd4cb
Sharanbabu1 <162317092+Sharanbabu1@users.noreply.github.com>
2024-04-14 00:16:04 +0800

    fix issue of sound manager

--- Assets/Sharan Adhikari/Scripts/SharanSoundManager.cs
+++ Assets/Sharan Adhikari/Scripts/SharanSoundManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundManager : MonoBehaviour
+public class SharanSoundManager : MonoBehaviour
 {
    public Sound[] sounds;
    
