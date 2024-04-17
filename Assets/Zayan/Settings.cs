f60de529cab46fd1b9314e9955a39def36ad1ee1
Zayanahamed <ahamedzayan99@gmail.com>
2024-04-18 02:28:13 +0800

    Zayan Assets
    Zayan Game

---
+++ Assets/Zayan/Settings.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Settings : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
